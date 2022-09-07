07fe7774178f1f8d0dd4b231ce158124a2dda5e1
kovacsv <viktorkovacs@gmail.com>
2022-09-07 20:05:38 +0200

    Remove temporary code from test.

--- archicad-addon/Examples/test.py
+++ archicad-addon/Examples/test.py
@@ -5,9 +5,6 @@ conn = ACConnection.connect ()
 acc = conn.commands
 act = conn.types
 
-response = acc.ExecuteAddOnCommand (act.AddOnCommandId ('TapirCommand', 'PublishPublisherSet'), {
-"publisherSetName" : "1 - Layouts",
-"outputPath" : "C:\\Users\\Viktor\\Documents\\Alma"
-})
+response = acc.ExecuteAddOnCommand (act.AddOnCommandId ('TapirCommand', 'GetAddOnVersion'))
 
 print (response)
